04a018f034f7478ea3855d3bf532a11406e5da69
Mayur <mayurpatle108@gmail.com>
2024-01-07 23:24:30 +0530

    initial

---
+++ script.js
@@ -0,0 +1,14 @@
+const header =  document.querySelector("header")  ; 
+const menuBtn  =  document.querySelector("#menu-btn")  ; 
+const  closeMenuBtn  = document.querySelector("#close-menu-btn")  ; 
+
+menuBtn.addEventListener("click" , ()=>{
+    header.classList.toggle("show-mobile-menu"); 
+
+
+})
+closeMenuBtn.addEventListener("click", () => {
+    header.classList.toggle("show-mobile-menu");    
+
+
+});
